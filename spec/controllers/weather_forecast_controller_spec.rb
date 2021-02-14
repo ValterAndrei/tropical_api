615086a882e02e0b63980e8b3658339e72dab162
Valter Andrei <valterandrey@gmail.com>
2021-02-14 13:28:12 -0300

    Add create tweet specs

--- spec/controllers/weather_forecast_controller_spec.rb
+++ spec/controllers/weather_forecast_controller_spec.rb
@@ -4,22 +4,63 @@ RSpec.describe WeatherForecastController, type: :request do
   describe 'GET #check_forecast' do
     before do
       allow(TropicalService).to receive(:run).and_return(
-        OpenStruct.new(data)
+        OpenStruct.new(payload)
       )
 
       get '/forecast'
     end
 
     context 'when found the city' do
-      let(:data) { { data: 'data_values', status: '200' } }
+      let(:payload) { { data: 'data_values', status: '200' } }
 
       it 'returns http success' do
         expect(response).to be_successful
       end
+
+      it 'return data values' do
+        expect(response.body).to eq(payload[:data])
+      end
     end
 
     context 'when city not was found' do
-      let(:data) { { data: 'data_error', status: '404' } }
+      let(:payload) { { data: 'data_error', status: '404' } }
+
+      it 'returns http not found' do
+        expect(response).to be_not_found
+      end
+
+      it 'return data values' do
+        expect(response.body).to eq(payload[:data])
+      end
+    end
+  end
+
+  describe 'POST #create_tweet' do
+    before do
+      allow(TropicalService).to receive(:run).and_return(
+        OpenStruct.new(payload)
+      )
+      allow(TwitterService).to receive(:send_tweet).and_return(
+        OpenStruct.new(id: 10)
+      )
+
+      post '/notification'
+    end
+
+    context 'when found the city' do
+      let(:payload) { { full_sumary: 'data_values', status: '200' } }
+
+      it 'returns http success' do
+        expect(response).to be_successful
+      end
+
+      it 'return data values' do
+        expect(JSON.parse(response.body)).to eq({ 'tweet_id' => 10, 'message' => 'data_values' })
+      end
+    end
+
+    context 'when city not was found' do
+      let(:payload) { { data: 'data_error', status: '404' } }
 
       it 'returns http success' do
         expect(response).to be_not_found
