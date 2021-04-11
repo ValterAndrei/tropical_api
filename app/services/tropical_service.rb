bc0b432b7d76c444be9713a5e8222dc6499e6f8d
Valter Andrei <valterandrey@gmail.com>
2021-04-11 13:01:16 -0300

    Refactor call to gem tropical

--- app/services/tropical_service.rb
+++ app/services/tropical_service.rb
@@ -2,13 +2,11 @@ class TropicalService
   class << self
     def run(params)
       Tropical::OpenWeatherMap.new(
-        {
-          q:     params['q'],
-          lang:  params['lang'] || 'pt_br',
-          units: params['units'] || 'metric',
-          cnt:   params['cnt'] || '0',
-          appid: ENV['TROPICAL_APPID']
-        }
+        q:     params['q'],
+        lang:  params['lang'] || 'pt_br',
+        units: params['units'] || 'metric',
+        cnt:   params['cnt'] || '0',
+        appid: ENV['TROPICAL_APPID']
       )
     end
   end
